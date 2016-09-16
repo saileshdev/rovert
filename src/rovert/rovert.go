152b9cd1c991d367b8b6970944839b8df3b02fc7
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-16 23:01:39 +0530

    Initialize the package with template caching

--- src/rovert/rovert.go
+++ src/rovert/rovert.go
@@ -12,6 +12,12 @@ var (
   templates *template.Template
 )
 
+func init() {
+  templates = template.Must(
+    template.ParseFiles("/Users/saileshdev/Desktop/rovert/static/root.html", "/Users/saileshdev/Desktop/rovert/static/editor.html"),
+  )
+}
+
 
 func HandleRoot(w http.ResponseWriter, r* http.Request) {
     templates.ExecuteTemplate(w, "root.html", nil)
