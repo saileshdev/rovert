fa8d82416ed5a4aee3ea1011432383ca07dad0de
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-16 23:30:21 +0530

    Satisfy the Transformer interface by implementing Transform method

--- src/transform/invert.go
+++ src/transform/invert.go
@@ -11,3 +11,13 @@ func init() {
   i := new(Invert)
   RegisterTransformer("invert", i)
 }
+
+func (r Invert) Transform(i image.Image) image.Image {
+  inverted := image.NewRGBA(i.Bounds())
+  for y := i.Bounds().Min.Y; y < i.Bounds().Max.Y; y++ {
+    for x := i.Bounds().Min.X; x < i.Bounds().Max.X; x++ {
+      inverted.Set(x, y, r.invert(i.At(x, y)))
+    }
+  }
+  return inverted.SubImage(i.Bounds())
+}
